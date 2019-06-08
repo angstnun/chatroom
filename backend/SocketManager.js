97ef708602dae450af2655034c1cdffb64f19ee9
Stef Nunez <angstnun@gmail.com>
2019-06-08 03:08:27 -0400

    Switched from Jade to EJS and set up socket.io

--- backend/SocketManager.js
+++ backend/SocketManager.js
@@ -1,5 +1,8 @@
 module.exports = (io) => {
     io.on('connection', function (socket) {
         console.log('a user connected');
+        socket.on('chat message', (message) => {
+            io.emit('chat message', message);
+        });
     });
 }
